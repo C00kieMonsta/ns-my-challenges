c37d73d01ddac743de89b05963f7418b4c2240d2
Antoine Boxho <karagan.app@gmail.com>
2019-03-14 00:07:20 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ChallengeEditComponent } from './challenges/challenge-edit/challenge-ed
 import { AuthComponent } from './auth/auth.component';
 import { TodayComponent } from './challenges/today/today.component';
 import { AppRoutingModule } from "./app-routing.module";
+import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 
@@ -30,6 +31,7 @@ import { AppRoutingModule } from "./app-routing.module";
         ChallengeEditComponent,
         AuthComponent,
         TodayComponent,
+        ActionBarComponent,
     ],
     providers: [],
     schemas: [
--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
-import { isAndroid } from 'tns-core-modules/platform';
 import { RouterExtensions } from 'nativescript-angular/router';
-import { Page } from 'tns-core-modules/ui/page/page';
-
-
-// this is a variable that is accessible everywhere in your adroidn code
-// but typescript doesn't know that it is globally available so we have to
-// declare it
-declare var android: any;
 
 @Component({
  selector: 'ns-current-challenge',
@@ -17,31 +9,9 @@ declare var android: any;
 })
 export class CurrentChallengeComponent {
 
-    constructor(
-        private router: RouterExtensions,
-        private page: Page,
-    ) {}
+    constructor(private router: RouterExtensions) {}
 
     onEdit() {
         this.router.navigate(['/edit-challenge']);
     }
-
-    // The Android back button color cannot be changed easily, we have to do it like this
-    // We call this function on the "loaded" event of the action bar
-    onLoadedActionBar() {
-        if (isAndroid) {
-            const androidToolBar = this.page.actionBar.nativeView;
-            const backButton = androidToolBar.getNavigationIcon();
-            if (backButton) {
-                /**
-                 * This is native code you have to call through the Javascript bridge
-                 */
-                backButton.setColorFilter(
-                    android.graphics.Color.parseColor('#888888'),
-                    (<any>android.graphics).PorterDuff.Mode.SRC_ATOP // this controls how color filter is applied
-                );
-            }
-        }
-    }
-
 }
