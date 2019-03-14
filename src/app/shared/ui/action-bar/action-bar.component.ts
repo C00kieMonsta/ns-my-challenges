24942453414b333e3ea640ead1a62cface64d40a
Antoine Boxho <karagan.app@gmail.com>
2019-03-14 01:14:09 +0100

    overwrite transition effect + refactor action bar shared component

--- src/app/shared/ui/action-bar/action-bar.component.ts
+++ src/app/shared/ui/action-bar/action-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { isAndroid } from 'tns-core-modules/platform';
 import { Page } from 'tns-core-modules/ui/page/page';
+import { RouterExtensions } from 'nativescript-angular/router';
 
 // this is a variable that is accessible everywhere in your adroidn code
 // but typescript doesn't know that it is globally available so we have to
@@ -16,10 +17,23 @@ declare var android: any;
 export class ActionBarComponent implements OnInit {
 
     @Input() title: string = 'Home';
+    @Input() showBackButton: boolean = true;
 
-    constructor(private page: Page) { }
+    constructor(
+        private page: Page,
+        private router: RouterExtensions
+    ) { }
 
     ngOnInit() {}
+
+    // 'get' means we can use it as a propert
+    get canGoBack() {
+        return this.router.canGoBack() && this.showBackButton;
+    }
+
+    onGoBack() {
+        this.router.backToPreviousPage();
+    }
 
     // The Android back button color cannot be changed easily, we have to do it like this
     // We call this function on the "loaded" event of the action bar
