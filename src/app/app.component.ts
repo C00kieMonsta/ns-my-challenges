4542e29a9c8dbf0e14dbaf0b687371d0e1f7e35e
Antoine Boxho <karagan.app@gmail.com>
2019-03-14 20:46:53 +0100

    update README + add button for sidedrawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "ns-app",
     moduleId: module.id,
+    styleUrls: ['./app.component.css'],
     templateUrl: "./app.component.html"
 })
 export class AppComponent {
