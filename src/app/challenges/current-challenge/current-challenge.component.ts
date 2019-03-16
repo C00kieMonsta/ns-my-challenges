714f204e10b785547753a386814f11eeec02be4e
Antoine Boxho <karagan.app@gmail.com>
2019-03-16 02:39:55 +0100

    WIP

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef } from '@angular/core';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
 import { ModalDialogService } from 'nativescript-angular/modal-dialog';
 
 import { DayModalComponent } from '../day-modal/day-modal.component';
@@ -13,16 +13,44 @@ import { UIService } from '~/app/shared/ui/ui.service';
     ],
     moduleId: module.id,
 })
-export class CurrentChallengeComponent {
+export class CurrentChallengeComponent implements OnInit {
 
     weekDays = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+    days: { dayInMonth: number, dayInWeek: number }[] = [];
+    today: Date;
 
     constructor(
         private modalDialog: ModalDialogService,
         private vcRef: ViewContainerRef,
         private uiService: UIService,
-    ) {}
+    ) {
+        this.today = new Date();
+    }
 
+    ngOnInit() {
+        const currentYear = this.today.getFullYear();
+        const currentMonth = this.today.getMonth();
+
+        /**
+         * When we set the day to 0, this is the last day of the last month because we set month+1
+         *
+         * For instance, we are in January, 2019
+         *
+         *  new Date(2019, (Jan + 1 = Feb), 0)  =  this gives us the last day of Jan  =  31
+         */
+        const numberOfDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+        for (let i = 0; i < numberOfDaysInMonth + 1; i++) {
+            const date = new Date(currentYear, currentMonth, i);
+            this.days.push({ dayInMonth: i, dayInWeek: date.getDay() });
+        }
+    }
+
+    getRow(i: number, day: { dayInMonth: number, dayInWeek: number }) {
+        const startow = 1; // row 0 is taken for week days
+        const weekRow = Math.floor(i / 7);
+        return startow + weekRow;
+    }
 
     onChangeStatus() {
         /**
